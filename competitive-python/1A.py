7985c828a17b886886ba6541bbc89109ad4db245
Ronak Jain <ronakbjain.2011@gmail.com>
2015-05-30 23:58:03 +0530

    Codeforces files added

---
+++ competitive-python/1A.py
@@ -0,0 +1,3 @@
+import re
+n,m,a=map(int,raw_input().split())
+print-n/a*(-m/a)
---
+++ competitive-python/4A.py
@@ -0,0 +1,7 @@
+#  map(int,raw_input().split())
+
+a = map(int,raw_input().split())
+if(a>2 & a%2==0):
+    print "YES"
+else:
+    print "NO"
